a08074e06a3b8a5be406a6f26a96508c654f1d43
YulinChan <chenyulin@yandex.com>
2021-02-25 23:10:10 +0800

    init repo

---
+++ setup.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import requests
+
+
+def list_repos(token, page=1):
+    url = 'https://api.github.com/user/repos'
+    header = {'Authorization': f'token {token}'}
+    params = {'per_page': 100, 'page': page}
+    r = requests.get(url, headers=header, params=params)
+    repos = r.json()
+    name_list = []
+    url_list = []
+    for repo in repos:
+        name_list.append(repo['name'])
+        url_list.append(repo['html_url'])
+    return zip(name_list, url_list)
+
+
+def gen_page(name):
+    if os.path.exists(f'./page/{name}.html'):
+        os.remove(f'./page/{name}.html')
+    hls = open('hls.html', 'r')
+    page = open(f'./page/{name}.html', 'a')
+    content = hls.read().replace('{name}', name)
+    page.write(content)
+    hls.close()
+    page.close()
+    print(name, 'page generated!')
+
+
+def gen_index(name, info):
+    if os.path.exists(f"{name}.html"):
+        os.remove(f"{name}.html")
+    exclude_repos = ['img', 'ghcdn.github.io', 'shixian']
+    head = """<html lang="zh-CN"><head><meta charset="UTF-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <link href="./css/main.css" rel="stylesheet"><title>My Video</title></head><body>
+           """
+    html = open(f'{name}.html', 'a')
+    html.write(head)
+    html.write('<div id="content" >')
+    for name, url in info:
+        if name in exclude_repos:
+            continue
+        html.write(f'<div id="{name}" class="video_info">')
+        r = requests.head(
+            f"https://cdn.jsdelivr.net/gh/ghcdn/{name}/img/pic0.jpg")
+        if r.status_code == 200:
+            html.write(
+                f'<img src="https://cdn.jsdelivr.net/gh/ghcdn/{name}/img/pic0.jpg">')
+        else:
+            html.write(f'<img src="https://cdn.jsdelivr.net/gh/ghcdn/{name}/pic0.jpg">')
+        html.write(f'<a href="{url}">{name}</a>  <a href="./page/{name}.html">Play Now</a>')
+        html.write('</div>')
+        print(name, "add index!")
+        gen_page(name)
+    html.write('</div></body></html>')
+    html.close()
+
+
+if __name__ == '__main__':
+    token = sys.argv[1]
+    info = list_repos(token)
+    gen_index('index',info)
